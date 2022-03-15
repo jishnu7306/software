8fccde6f8db4b6a4d2b5381838ef1a2e73a672e8
jishnu <jishnuksatheeshsj143@gmail.com>
2022-03-15 12:30:56 +0530

    jishnu

--- app/views.py
+++ app/views.py
@@ -2,6 +2,31 @@ from django.shortcuts import render
 
 # Create your views here.
 
+#***********************  client  *******************************************************
+def client_Dashboard(request):
+    return render(request,'client\client_Dashboard.html')
+
+def base_client(request):
+     return render(request,'client\base_client.html')
+
+def client_project_details(request):
+    return render(request,'client\client_project_details.html')
+
+def manager_attendance_show(request):
+    return render(request,'manager\manager_attendance_show.html')
+
+def client_company_card(request):
+    return render(request,'client\client_company_card.html')
+
+def client_company_details1(request):
+    return render(request,'client\client_company_details1.html')
+
+def client_company_details2(request):
+    return render(request,'client\client_company_details2.html')
+
+def client_company_details3(request):
+    return render(request,'client\client_company_details3.html')
+
 
 #***********************developer***********************************************
 def index(request):
--- software/urls.py
+++ software/urls.py
@@ -6,6 +6,18 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('index', views.index, name='index'),
 
+    #*************************  client  ****************************
+    path('base_client',views.base_client),
+    path('client_Dashboard',views.client_Dashboard),
+    path('client_project_details',views.client_project_details),
+    path('client_company_card',views.client_company_card),
+    path('client_company_details1',views.client_company_details1),
+    path('client_company_details2',views.client_company_details2),
+    path('client_company_details3',views.client_company_details3),
+
+
+
+
 
     
 
